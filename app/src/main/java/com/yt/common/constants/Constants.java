83ddf76bc1fd7a692550f2af61de591b96d84b19
tauqeer <tauqeer007@gmail.com>
2015-08-16 20:29:18 +0530

    changed device id to emulator

--- app/src/main/java/com/yt/common/constants/Constants.java
+++ app/src/main/java/com/yt/common/constants/Constants.java
@@ -2,7 +2,7 @@ package com.yt.common.constants;
 
 public class Constants {
 
-	public static final String PREFS_DEVICE_ID = "4790136500605000";
+	public static final String PREFS_DEVICE_ID = "emulator-5554";
 	public static final String USER_AGENT_PATTERN = "Mozilla/5.0 (Linux; U; Android %1$s; %2$s; %3$s Build/%4$s) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
 	public static final boolean STARTAPP_FULL_SCREEN_ADS = true;
 	public static final boolean STARTAPP_BANNER_ADS = false;
